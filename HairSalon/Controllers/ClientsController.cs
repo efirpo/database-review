b47a4c925c9a4a9e762b8fd098670c8292da6993
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-05-29 10:14:07 -0700

    add create view, controller for clients

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,32 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Details", "Stylists", new { id = client.StylistId });
+    }
+  }
+}
